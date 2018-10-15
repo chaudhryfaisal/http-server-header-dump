98903367c12473053a96f5eef9aa52503a44065f
Faisal Chaudhry <faisal.chaudhry@mastercard.com>
2018-10-15 14:32:37 -0500

    auto commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	r.Write(w)
+	fmt.Println(r)
+	fmt.Fprintf(w, "RemoteAddr: %s", r.RemoteAddr)
+}
+
+func main() {
+	port := flag.Int("port", 8080, "Listen on port")
+	flag.Parse()
+
+	http.HandleFunc("/", handler)
+	log.Println("Starting http server on port:", *port)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
+}
