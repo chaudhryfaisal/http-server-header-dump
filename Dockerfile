b0df55bc553169b0f4b1d9928a37a53dccfb920d
Chaudhry, Faisal <Faisal.Chaudhry@mastercard.com>
2020-06-24 15:40:09 -0500

    Added Makefile Added options to print / filter ENV variables

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,20 @@
+############################
+# STEP 1 build executable binary
+############################
+FROM golang:alpine AS builder
+WORKDIR $GOPATH/src/app
+COPY main.go .
+# Using go get.
+RUN go get -d -v
+# Build the binary.
+RUN GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o /go/bin/app
+############################
+# STEP 2 build a small image
+############################
 FROM alpine
 EXPOSE 8080
-ENV PORT 8080
-COPY bin/http-server-header-dump-linux-amd64 /
-CMD /http-server-header-dump-linux-amd64 --port $PORT
+ENV PORT=8080
+ENV DUMP_ENV=false
+ENV ENV_MATCH=".*"
+COPY --from=builder /go/bin/app /app
+ENTRYPOINT /app --port=$PORT --includeEnv=$DUMP_ENV --matchEnv=$ENV_MATCH
--- main.go
+++ main.go
@@ -6,25 +6,52 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"os"
+	"regexp"
 	"strconv"
+	"strings"
 	"time"
 )
 
-func Handler(w http.ResponseWriter, req *http.Request) {
+var dumpEnv = false
+var envMatch = ".*"
+
+func Handler(resp http.ResponseWriter, req *http.Request) {
 	requestDump, err := httputil.DumpRequest(req, true)
 	if err != nil {
 		fmt.Println("Errror:", err)
 	}
 	fmt.Println(string(requestDump))
 	fmt.Printf("------------------------- %s -------------------------\n", time.Now().Format("2006-01-02 3:4:5 PM"))
-	req.Write(w)
+	if dumpEnv {
+		for _, e := range os.Environ() {
+			pair := strings.SplitN(e, "=", 2)
+			res1, _ := regexp.MatchString(envMatch, pair[0])
+			if res1 {
+				req.Header.Add(fmt.Sprintf("ENV_%s", pair[0]), pair[1])
+			}
+		}
+	}
+	req.Write(resp)
 }
 
 func main() {
 	port := flag.Int("port", 8080, "Listen on port")
+	includeEnv := flag.Bool("includeEnv", dumpEnv, "Include Environment")
+	matchEnv := flag.String("matchEnv", envMatch, "Match ENV KEYs")
 	flag.Parse()
-
+	dumpEnv = *includeEnv
+	envMatch = *matchEnv
+	log.Printf("Starting http server port=%d includeEnv=%t matchEnv=%s", *port, dumpEnv, envMatch)
+	if dumpEnv {
+		for _, e := range os.Environ() {
+			pair := strings.SplitN(e, "=", 2)
+			res1, _ := regexp.MatchString(envMatch, pair[0])
+			if res1 {
+				log.Printf("ENV: %s=%s\n", pair[0], pair[1])
+			}
+		}
+	}
 	http.HandleFunc("/", Handler)
-	log.Println("Starting http server on port:", *port)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
 }
