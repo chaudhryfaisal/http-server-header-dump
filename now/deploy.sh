84bdfbd8b5d501f449f322f0d38d69877efef5e1
Chaudhry, Faisal <Faisal.Chaudhry@mastercard.com>
2021-05-17 13:10:22 -0400

    auto commit

--- now/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p api
-cp ../main.go api
-sed -i '' 's/func main/func old_main/g' api/main.go
-cat api/main.go
-./now.sh --prod "$@"
-rm -rf api
--- now/now.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-now -t RSIXTMfxy4ByztP9nl5bhnZy -d "$@"
